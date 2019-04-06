2069a86beeeccebeeb3040d29213952ee859d297
Ognian <deqnow@gmail.com>
2019-04-06 18:15:42 +0200

    Initial commit

---
+++ src/main/java/library/entities/Book.java
@@ -0,0 +1,58 @@
+package library.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Min(value = 1)
+    private double price;
+
+    public Book(String title, String author, @Min(value = 1) double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
